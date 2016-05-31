ae4b22a5091e03691e46cfaa0b39f9a439b69bbd
mrjeki <git@schneewind.ch>
2016-05-31 20:21:56 +0200

    backup

---
+++ PlzTime/Model/DatabaseModel.cs
@@ -0,0 +1,112 @@
+﻿#region "Internal Libraries"
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+#endregion
+#region "Importet Libraries"
+using SQLite;
+#endregion
+
+#region "Own Classes"
+#endregion
+
+namespace PlauschzeitfahrenTMS
+{
+	public class DatabaseModel
+	{
+		#region "### Properties #############################################"
+		private static string _dbFilename = "PlzTms.sqlite";
+
+		private string _dbPath;
+		private string _dbFile;
+		public SQLiteConnection _connection;
+		#endregion
+		#region "### Constructors #############################################"
+		public DatabaseModel ()
+		{	
+			if (!verifyDatabase()) {
+				Console.WriteLine ("DatabaseModel::DatabaseModel(): Database doesn't exist or is corrupt -> initDatabase()");
+				this.initDatabase ();
+			}
+			Console.WriteLine("DatabaseModel::DatabaseModel(): Database ready to use! " );
+		}
+		#endregion
+		#region "### Deconstructors #############################################"
+		#endregion
+		#region "### UI Methods #############################################"
+		#endregion
+		#region "### Private Methods #############################################"
+		private void initDatabase(){
+			try {
+				// IOS: ~/Documents > Only for user-created files, is sync with iTunes if supported.
+				string presonalFolder = Environment.GetFolderPath (Environment.SpecialFolder.Personal);
+				// IOS: Applicationfiles are be deposed in ~/Library/<subfolder>/<files>
+				// 		This is required by Apple!
+				_dbPath = System.IO.Path.Combine(presonalFolder, ".." , "Library/Application Support/" , "Plauschzeitfahren TMS");
+
+				if (!System.IO.Directory.Exists (_dbPath)){
+						System.IO.Directory.CreateDirectory (_dbPath);
+				}
+				
+				// Create Database-File...
+				_dbFile = System.IO.Path.Combine(_dbPath, _dbFilename);
+				Console.WriteLine("DatabaseModel::initDatabase()._dbFile= "+_dbFile);
+
+				// Dreate Database-Structure...
+				this.connect();
+				var result = _connection.CreateTable<Person>();
+				Console.WriteLine("DatabaseModel::initDatabase().CreateTable<Participants>()= "+result);
+				this.disconnect();
+			} catch (Exception ex) {
+				Console.WriteLine("ERROR DatabaseModel::initDatabase(): "+ex.ToString());
+			}
+		}
+		#endregion
+		#region "### Public Methods #############################################"
+		public bool verifyDatabase(){
+			if(File.Exists(_dbFile)) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+		public void connect(){
+			try {
+				_connection = new SQLiteConnection (_dbFile);
+				Console.WriteLine("DatabaseModel::connect()");
+			} catch (Exception ex) {
+				Console.WriteLine("ERROR DatabaseModel::connect(): "+ex.ToString());
+			}
+		}
+		public void disconnect(){
+			try {
+				_connection.Close();
+				Console.WriteLine("DatabaseModel::disconnect()");
+			} catch (Exception ex) {
+				Console.WriteLine("ERROR DatabaseModel::disconnect(): "+ex.ToString());
+			}
+		}
+		public void deleteDatabase(){
+			try {
+				this.disconnect ();
+				if (File.Exists (_dbFile)) {
+					File.Delete (_dbFile);
+				}
+			} catch (Exception ex) {
+				Console.WriteLine ("ERROR DatabaseModel::deleteDatabase(): " + ex.ToString ());
+			}
+		}
+		public List<Person> getParticipantsOfRace () {
+				try {
+					this.connect ();
+					return this._connection.Table<Person>().ToList(); // Dangerous if list is big!
+				} catch (Exception ex) {
+					Console.WriteLine("ERROR DatabaseModel::getParticipantsOfRace(): "+ex.ToString());
+					return null;
+				}					
+		}
+				
+		#endregion
+	}
+}
---
+++ PlzTime/Model/Participant.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using SQLite;
+
+namespace PlauschzeitfahrenTMS
+{
+	[Table("Participant")]
+	public class Participant
+	{
+		[PrimaryKey, AutoIncrement, Column("ID")]
+		private int ID { get; set; }
+		[Column("creatmin_ts")]
+		private DateTime creation { get; set; }
+		[Column("modification_ts")]
+		private DateTime modification { get; set; }
+		[Indexed, NotNull]
+		public int plz_ID { get; set; }
+		public string gender { get; set; }
+		public string firstName { get; set; }
+		public string lastName { get; set; }
+		//[Ignore] // = is not stored in Database
+		public string name { get; }
+		public string birthday { get; set; }
+		public string street { get; set; }
+		public string number { get; set; }
+		public string zip { get; set; }
+		public string city { get; set; }
+		public string region { get; set; }
+		public string country { get; set; }
+		public string telephone { get; set; }
+		public string mobile { get; set; }
+		public string mail { get; set; }
+
+		public Participant(){
+			this.creation = DateTime.UtcNow;
+			this.modification = DateTime.UtcNow;
+		}
+
+		public Participant(bool p_test){
+			if (p_test)
+			{
+				this.creation = DateTime.UtcNow;
+				this.modification = DateTime.UtcNow;
+				this.plz_ID = 98001;
+				this.gender = "male";
+				this.firstName = "Loïc";
+				this.lastName = "Mérat";
+				this.name = this.firstName + " " + this.lastName;
+				this.birthday = "03-09-1918";
+				this.street = "Mont de Drôts";
+				this.number = "9s";
+				this.zip = "1450";
+				this.city = "Ste-Croix";
+				this.region = "VD";
+				this.country = "Schweiz";
+				this.telephone = "+41999999999";
+				this.mobile = "+41799999999";
+				this.mail = "toto@mac.com";
+			}
+		}
+	}
+}
+
